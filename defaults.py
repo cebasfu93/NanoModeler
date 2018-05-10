17aca803aa864b35a293c856f4bbd752d5877b59
Sebastian Franco Ulloa <cebasfu93@gmail.com>
2018-05-10 15:31:20 +0200

    Redundancy while writing TLeap.in removed

--- defaults.py
+++ defaults.py
@@ -2,14 +2,13 @@ VAR = {
 "NAME": "test",             #Name of the project
 "LIG1_FILE" : "LF1.mol2",   #Name of the mol2 of ligand1 (must be in the working directory)
 "CAP1" : "N",
-"LIG1_FRAC" : 1.0,          #Fraction of ligand1 to place
 
 "LIG2_FILE" : "LF2.mol2",   #Name of the mol2 of ligand2 (must be in the working directory)
 "CAP2" : "N",
 "MORPHOLOGY" : "random",    #Morphology to distribute ligands1 and 2. random, janus, and stripe are allowed
+"LIG1_FRAC" : 1.0,          #Fraction of ligand1 to place
 "RSEED" : 666,              #Random seed for random morphology
-"STRIPES" : 1,              #Number of stripes for stripe morphology
-"FIRST"   : 1,              #Ligand (1 or 2) to be put first in the stripe morphology
+"STRIPES" : 1,              #Number of stripes for stripe morphology. It will start (bottom up with ligand 1)
 
 "CORE" : "au144SR60_NM.pdb",
 
@@ -18,28 +17,26 @@ VAR = {
 }
 
 def write_leap(fname, two_lig_func):
-    msj = "source leaprc.gaff \n"
+    msj = "source leaprc.gaff \n\n"
     msj += "loadamberparams " + "TMP/"+VAR["LIG1_FILE"][:-5]+".frcmod\n"
     msj += VAR["LIG1_FILE"][:-5] + " = loadmol3 " + "TMP/"+VAR["LIG1_FILE"]+"\n"
     msj += "check " + VAR["LIG1_FILE"][:-5] + "\n"
-    msj += "saveoff " + VAR["LIG1_FILE"][:-5] + " " + "TMP/"+VAR["LIG1_FILE"][:-5]+".lib\n"
+    msj += "saveoff " + VAR["LIG1_FILE"][:-5] + " " + "TMP/"+VAR["LIG1_FILE"][:-5]+".lib\n\n"
     if two_lig_func:
         msj += "loadamberparams " + "TMP/"+VAR["LIG2_FILE"][:-5]+".frcmod\n"
         msj += VAR["LIG2_FILE"][:-5] + " = loadmol3 " + "TMP/"+VAR["LIG2_FILE"]+"\n"
         msj += "check " + VAR["LIG2_FILE"][:-5] + "\n"
-        msj += "saveoff " + VAR["LIG2_FILE"][:-5] + " " + "TMP/"+VAR["LIG2_FILE"][:-5]+".lib\n"
+        msj += "saveoff " + VAR["LIG2_FILE"][:-5] + " " + "TMP/"+VAR["LIG2_FILE"][:-5]+".lib\n\n"
 
     msj += "loadamberparams " + VAR["DEPENDS"]+"/AU.frcmod\n"
     msj += "loadamberparams " + VAR["DEPENDS"]+"/ST.frcmod\n"
     msj += "AU = loadmol3 " + VAR["DEPENDS"]+"/AU.mol2\n"
-    msj += "ST = loadmol3 " + VAR["DEPENDS"]+"/ST.mol2\n"
+    msj += "ST = loadmol3 " + VAR["DEPENDS"]+"/ST.mol2\n\n"
+
     msj += "loadoff " + "TMP/"+VAR["LIG1_FILE"][:-5]+".lib\n"
-    msj += "loadamberparams "+ "TMP/"+VAR["LIG1_FILE"][:-5]+".frcmod\n"
-    msj += VAR["LIG1_FILE"][:-5] + " = loadmol3 " + "TMP/"+VAR["LIG1_FILE"]+"\n"
     if two_lig_func:
         msj += "loadoff " + "TMP/"+VAR["LIG2_FILE"][:-5]+".lib\n"
-        msj += "loadamberparams "+ "TMP/"+VAR["LIG2_FILE"][:-5]+".frcmod\n"
-        msj += VAR["LIG1_FILE"][:-5] + " = loadmol3 " + "TMP/"+VAR["LIG2_FILE"]+"\n"
+
     msj += VAR["NAME"] + " = loadpdb " + "TMP/"+VAR["NAME"]+".pdb \n"
     msj += "saveamberparm " +  VAR["NAME"] + " " + "TMP/"+VAR["NAME"]+".prmtop" + " " + "TMP/"+VAR["NAME"]+".inpcrd \n"
     msj += "quit"
