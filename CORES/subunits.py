4825df9e6f2b2f1d9639a92933dc0245647e266e
Sebastian Franco Ulloa <cebasfu93@gmail.com>
2018-05-11 10:56:47 +0200

    cleaner

--- CORES/subunits.py
+++ CORES/subunits.py
@@ -22,22 +22,16 @@ class Staple:
     #A staple is as defined in paper 333
     def __init__(self, ndx_S=0, ndx_Au=0, ndx_C=0):
         #self.tipo = tipo
-        self.S = np.array(ndx_S, dtype='int')
+        self.S = np.unique(np.array(ndx_S, dtype='int'))
         #self.Au_l = np.array(ndx_Au_l, dtype='int')
         self.Au = np.array(ndx_Au, dtype='int')
         #self.Au_s = np.array(list(set(list(self.Au))-set(list(self.Au_l))), dtype='int')
-        self.C = np.array(ndx_C, dtype='int')
+        self.C = np.unique(np.array(ndx_C, dtype='int'))
         only = np.unique(self.Au, return_counts=True)
-        self.Au_l = only[0][np.where(only[1]==2)[0]]
+        self.Au_l = only[0][np.where(only[1]>=2)[0]]
         self.Au_s = only[0][np.where(only[1]==1)[0]]
         self.Au = only[0]
         self.tipo = "UNK"
 
     def change_tipo(self, new_tipo):
         self.tipo = new_tipo
-
-class Residue:
-    #A residue is only defined by the residue type and atomnumbers that conform the residue
-    def __init__(self, restype='UNK', ndx=[]):
-        self.restype = restype
-        self.ndx = ndx
