8bdcf4a9fe1d2a66c7f90ba480ee49045c9ae756
Sebastian Franco Ulloa <cebasfu93@gmail.com>
2018-05-25 10:20:08 +0200

    When crash, only deletes files that were written

--- DEPENDENCIES/cleanup.py
+++ DEPENDENCIES/cleanup.py
@@ -4,7 +4,8 @@ def cleanup_error(TMP, log):
     log += "Cleaning up...\n"
     bye = ["ANTECHAMBER.FRCMOD", "leap.log", "md.mdp", "em.mdp", "acpype.log"]
     for i in bye:
-        os.system("rm " + str(i))
+        if os.path.isfile(i):
+            os.remove(i)
     #os.system("rm -r " + TMP)
     log += "NanoModeler terminated with errors.\n"
     print(log)
